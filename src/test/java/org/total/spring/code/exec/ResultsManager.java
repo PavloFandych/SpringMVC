930007811e56f7451f3d917e01531c8045c7a12d
pavlo.fandych <totalpasha@gmail.com>
2016-11-21 18:27:22 +0200

    Client activity started

---
+++ src/test/java/org/total/spring/code/exec/ResultsManager.java
@@ -0,0 +1,14 @@
+package org.total.spring.code.exec;
+
+import org.total.spring.code.finder.DataFinder;
+import org.total.spring.code.finder.DataFinderBundesLiga1;
+
+/**
+ * Created by pavlo.fandych on 11/21/2016.
+ */
+public class ResultsManager {
+    public static void main(String[] args) {
+        DataFinder dataFinder = new DataFinderBundesLiga1();
+        System.out.println(dataFinder.findResults());
+    }
+}
--- src/test/java/org/total/spring/code/finder/DataFinderBundesLiga1.java
+++ src/test/java/org/total/spring/code/finder/DataFinderBundesLiga1.java
@@ -1,5 +1,6 @@
 package org.total.spring.code.finder;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.total.spring.code.enums.Protocol;
@@ -8,6 +9,7 @@ import org.total.spring.root.entity.Result;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -22,7 +24,7 @@ public class DataFinderBundesLiga1 extends DataFinder {
     }
 
     @Override
-    protected Set<Result> findResults() {
+    public Set<Result> findResults() {
         Set<Result> results = new HashSet<>();
         try {
             String response = HttpExecutor.getInstance()
@@ -30,7 +32,14 @@ public class DataFinderBundesLiga1 extends DataFinder {
 
             JSONObject jsonObject = (JSONObject) new JSONParser().parse(response);
 
-            LOGGER.debug(jsonObject);
+            JSONArray fixtues = (JSONArray)jsonObject.get("fixtures");
+
+            Iterator<JSONObject> iterator = fixtues.iterator();
+            while (iterator.hasNext()) {
+                JSONObject object = iterator.next();
+                System.out.println("AWAY " + object.get("awayTeamName") + " HOME " + object.get("homeTeamName"));
+            }
+
         } catch (Exception e) {
             LOGGER.error(e, e);
         }
