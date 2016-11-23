dcd7f9735189cb16bfa696a53a62f71db7450ab6
pavlo.fandych <totalpasha@gmail.com>
2016-11-23 18:23:35 +0200

    getTeamByTeamId  stored proc added

---
+++ src/test/resources/sql/GoalDB_Scripts/storedProcedures/getTeamByTeamId.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS getTeamByTeamId;
+$$
+
+CREATE PROCEDURE getTeamByTeamId(IN teamId         BIGINT)
+  BEGIN
+    SELECT
+      *
+    FROM Team
+    WHERE Team.teamId = teamId;
+  END$$
+
+DELIMITER ;
