f7b26d9445dafe5506a649706bb9f11ff4168d0b
Pavlo Fandych <pavlo.fandych@synapse.com>
2017-11-08 19:32:49 +0200

    h2 version updated

--- src/test/java/org/total/spring/root/dao/CouplesDAOTest.java
+++ src/test/java/org/total/spring/root/dao/CouplesDAOTest.java
@@ -1,12 +1,13 @@
 package org.total.spring.root.dao;
 
 import org.junit.Test;
+import org.total.spring.repository.AbstractRepositoryDBUnitTest;
 
 /**
  * @author Pavlo.Fandych
  */
 
-public class CouplesDAOTest {
+public class CouplesDAOTest extends AbstractRepositoryDBUnitTest {
 
     @Test
     public void test() throws Exception {
