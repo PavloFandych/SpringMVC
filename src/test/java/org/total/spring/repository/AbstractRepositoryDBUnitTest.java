2ed839a0bf32b0d0d413b152beb98868ba6f88f0
TotalP <l777Pui1>
2018-01-04 14:40:01 +0200

    libs' versions have been upgraded

--- src/test/java/org/total/spring/repository/AbstractRepositoryDBUnitTest.java
+++ src/test/java/org/total/spring/repository/AbstractRepositoryDBUnitTest.java
@@ -5,8 +5,11 @@ import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import org.apache.commons.io.Charsets;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.h2.H2DataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.h2.tools.RunScript;
 import org.junit.Before;
@@ -54,21 +57,26 @@ public abstract class AbstractRepositoryDBUnitTest {
         final IDataSet dataSet = new FlatXmlDataSetBuilder()
                 .build(new File(configProperties.getProperty(getDataSetName())));
 
-        IDatabaseTester databaseTester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+        final IDatabaseTester databaseTester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
                 configProperties.getProperty("h2dbUrl"),
                 configProperties.getProperty("h2dbUsername"),
                 configProperties.getProperty("h2dbPassword"));
         databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
         databaseTester.setDataSet(dataSet);
         databaseTester.onSetup();
+
+        final IDatabaseConnection connection = databaseTester.getConnection();
+        final DatabaseConfig config = connection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+
         setUpIsDone = true;
     }
 
-    private String getSchemaName() {
+    private static String getSchemaName() {
         return "schema";
     }
 
-    private String getDataSetName() {
+    private static String getDataSetName() {
         return "dataset";
     }
 }
